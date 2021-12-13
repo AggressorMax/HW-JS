3134e9139bf384fe7232f55a30241fecc55c5c5e
MAKSYM LAPTIEV <92050561+AggressorMax@users.noreply.github.com>
2021-12-13 17:51:09 +0200

    Add files via upload

--- Debit-Credit-Laptiev-JS/js/main.js
+++ Debit-Credit-Laptiev-JS/js/main.js
@@ -1,78 +1,68 @@
+let dates = [
+    '02.12.21',
+    '04.12.21',
+    '08.12.21',
+    '10.12.21',
+    '12.12.21',
+    '14.12.21',
+];
 let debet = [
-    {name: 'ЗП', summ: 200, data: '02.12.21'},
-    {name: 'Халтура', summ: 1000, data: '04.12.21'},
-    {name: 'Лототрон', summ: 10000, data: '08.12.21'},
-    {name: 'Казино', summ: 13000, data: '10.12.21'},
-    {name: 'Подарок', summ: 5000, data: '12.12.21'},
-    {name: 'Нашел', summ: 500, data: '14.12.21'},
+    {name: 'ЗП', summ: 200, date: '02.12.21'},
+    {name: 'доп процент', summ: 500, date: '02.12.21'},
+    {name: 'Халтура', summ: 1000, date: '04.12.21'},
+    {name: 'доп процент', summ: 300, date: '04.12.21'},
+    {name: 'Лототрон', summ: 10000, date: '08.12.21'},
+    {name: 'доп процент', summ: 440, date: '08.12.21'},
+    {name: 'Казино', summ: 13000, date: '10.12.21'},
+    {name: 'доп процент', summ: 250, date: '10.12.21'},
+    {name: 'Подарок', summ: 5000, date: '12.12.21'},
+    {name: 'доп процент', summ: 564, date: '12.12.21'},
+    {name: 'Нашел', summ: 500, date: '14.12.21'},
+    {name: 'доп процент', summ: 0, date: '14.12.21'},
 ];
 
+
 let credit = [
-    {name: 'Продукты', summ: 1000, data: '02.12.21'},
-    {name: 'Шопинг', summ: 10000, data: '04.12.21'},
-    {name: 'Кино', summ: 500, data: '08.12.21'},
-    {name: 'Кафе', summ: 1500, data: '10.12.21'},
-    {name: 'Авто', summ: 4000, data: '12.12.21'},
-    {name: 'Спорт товары', summ: 2000, data: '14.12.21'},
-]
+    {name: 'Продукты', summ: 1000, date: '02.12.21'},
+    {name: 'проезд', summ: 16, date: '02.12.21'},
+    {name: 'Шопинг', summ: 1500, date: '04.12.21'},
+    {name: 'проезд', summ: 16, date: '04.12.21'},
+    {name: 'Кино', summ: 500, date: '08.12.21'},
+    {name: 'закуски', summ: 500, date: '08.12.21'},
+    {name: 'проезд', summ: 16, date: '08.12.21'},
+    {name: 'Кафе', summ: 1500, date: '10.12.21'},
+    {name: 'проезд', summ: 16, date: '10.12.21'},
+    {name: 'Авто', summ: 4000, date: '12.12.21'},
+    {name: 'переобувка резины', summ: 1000, date: '12.12.21'},
+    {name: 'проезд', summ: 16, date: '12.12.21'},
+    {name: 'Спорт товары', summ: 2000, date: '14.12.21'},
+    {name: 'проезд', summ: 16, date: '14.12.21'},
+];
+
+let bonus = debet.filter (function(item){
+    return item.name === 'доп процент';
+});
+console.log(bonus);
+let arr = credit.filter (function(item){
+    return item.summ === 16;
+});
+console.log(arr);
 
 
-
-for (let key in debet, credit) {
-    console.log(`${debet[key].data}`);
-    console.log('прихрд с: ');
-    console.log(`${debet[key].name}: ${debet[key].summ}грн.`);
-    console.log('Расход');
-    console.log(`${credit[key].name}: ${credit[key].summ}грн.`);
-    //console.log(student[key].value);
-}
-
-
-/*if (debet) {
-    console.log();
-}
-
-
-let income = chekOrgIncome('Халтура');
-if (income) {
-    console.log(`${income.data}`);
-    console.log('Приход:');
-    console.log(`${income.name},${income.summ}грн.`);
-}
-
-let expence = chekOrgExpence('АТБ');
-if (expence) {
-    console.log(`${expence.data}`);
-    console.log('Расход:');
-    console.log(`${expence.name},${expence.summ}грн.`);
-}
-
-
-
-
-
-
-
-function chekOrgIncome(name) {
-    for (let i = 0; i < debet.length; i++) {
-        let income = debet[i];
-        return income;
+for (let i = 0; i < dates.length; i++) {
+    let currentDate = dates[i];
+    console.log(`date - ${currentDate}`);
+    console.log('Debet');
+    for (let d = 0; d < debet.length; d++ ) {
+        if (currentDate === debet[d].date) {
+            console.log(`${debet[d].name}: ${debet[d].summ}`);
+        }
     }
-}
-function chekOrgExpence(name) {
-    for (let i = 0; i < credit.length; i++) {
-        let expence = credit[i];
-        
-        return expence;
+    console.log('Credit');
+    for (let c = 0; c < credit.length; c++ ) {
+        if (currentDate === credit[c].date) {
+            console.log(`${credit[c].name}: ${credit[c].summ}`);
+        }
     }
-}*/
-
-/*
-02.12.21
-Приход
-ЗП: 200
-Расход:
-АТБ: 200
-Кино: 500
-
-*/
+    console.log('**********');
+}
